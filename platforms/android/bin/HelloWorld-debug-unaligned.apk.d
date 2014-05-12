bbe7ce0080ecc405ce5dcc386f5fc632a515ba2f
gdmec07120747 <358874678@qq.com>
2014-05-12 11:38:15 +0800

    testplugin

---
+++ platforms/android/bin/HelloWorld-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\testplugin\testplugin\platforms\android\bin\HelloWorld-debug-unaligned.apk : \
+C:\testplugin\testplugin\platforms\android\bin\HelloWorld.ap_ \
+C:\testplugin\testplugin\platforms\android\bin\classes.dex \
+C:\testplugin\testplugin\platforms\android\CordovaLib\bin\classes.jar \
---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,9 @@
+C:\testplugin\testplugin\platforms\android\bin\classes.dex : \
+C:\testplugin\testplugin\platforms\android\bin\classes\com\phonegap\helloworld\BuildConfig.class \
+C:\testplugin\testplugin\platforms\android\bin\classes\com\phonegap\helloworld\HelloWorld.class \
+C:\testplugin\testplugin\platforms\android\bin\classes\com\phonegap\helloworld\R$attr.class \
+C:\testplugin\testplugin\platforms\android\bin\classes\com\phonegap\helloworld\R$drawable.class \
+C:\testplugin\testplugin\platforms\android\bin\classes\com\phonegap\helloworld\R$string.class \
+C:\testplugin\testplugin\platforms\android\bin\classes\com\phonegap\helloworld\R$xml.class \
+C:\testplugin\testplugin\platforms\android\bin\classes\com\phonegap\helloworld\R.class \
+C:\testplugin\testplugin\platforms\android\bin\dexedLibs\classes-69f5361338b638909ac82b1eaeb7fbc2.jar \
